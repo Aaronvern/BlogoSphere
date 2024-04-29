5440a893dbe1832021f9ffbace247a71c426840a
aaronvern <aaronvernekar@gmail.com>
2024-04-29 20:37:16 +0530

    add config

--- BlogoSphere/src/config/config.js
+++ BlogoSphere/src/config/config.js
@@ -1,3 +1,9 @@
-const config = {}
+const config = {
+    appwriteUrl:import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    appwriteProjectId:import.meta.env.VITE_APPWRITE_PROJECT_ID,
+    appwriteDatabaseId:import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    appwriteBucketId:import.meta.env.VITE_APPWRITE_BUCKET_ID,
+    appwriteCollectionId:import.meta.env.VITE_APPWRITE_COLLECTION_ID,
+}
 
 export default config
