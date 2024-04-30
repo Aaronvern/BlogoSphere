e762a1788a1cb95cc4688560c3694cd6cf1c8e05
aaronvern <aaronvernekar@gmail.com>
2024-04-30 16:00:09 +0530

    add appwrite service for database operations

---
+++ BlogoSphere/src/appwrite/conf.js
@@ -0,0 +1,103 @@
+import config from "../config/config.js";
+
+import { Client, Account, ID , Databases , Query } from "appwrite";
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+    constructor(){
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId)
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(this.client)
+    }
+    async createPost( {title,content,featuredImage,status,userid}){
+        try{
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userid
+                }
+            )
+        }catch(error){
+            console.log("appwrite service :: createPost :: error",error)
+        }
+    }
+    async updatePost(slug,{title,content,featuredImage,status}){
+        try{
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            );
+        }catch(error){
+            console.log("appwrite service :: updatePost :: error",error)
+        }
+    }
+    async deletPost(slug){
+        try{
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true;
+        }catch(error){
+            console.log("appwrite service :: deletepost :: error", error)
+            return false
+        }
+        
+    }
+    async getPost(slug){
+        try{
+            await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            );
+            return true
+        }catch(error){
+            console.log("appwrite service :: getPost :: error",error)
+            return false
+        }
+    }
+    async getPosts(queries = [Query.equal("status","active")]){
+        try{
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries,
+            )
+        }catch(error){
+            console.log("appwrite service:: getpPosts :: error ",error)
+            return false;
+        }
+    }
+
+    //file upload services
+    async uploadFile(file){
+        try {
+
+        }catch(error){
+            console.log("appwrite services:: uploadFile :: error",error)
+        }
+    }
+}
+
+const service = new Service();
+
+export default service
