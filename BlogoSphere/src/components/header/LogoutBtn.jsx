84c795a2026fc1d832b32644ac158c5975eb6e12
aaronvern <aaronvernekar@gmail.com>
2024-05-01 01:21:10 +0530

    add footer component

---
+++ BlogoSphere/src/components/header/LogoutBtn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useDispatch} from 'react-redux'
+import AuthService from '../../appwrite/auth.js'
+import {authSlice} from '../../features/auth/authSlice.js'
+const LogoutBtn = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default LogoutBtn;
