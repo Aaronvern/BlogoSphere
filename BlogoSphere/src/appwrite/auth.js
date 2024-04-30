42ed8f3cb6c90b419c9b2d0eaf296a1918bc4176
aaronvern <aaronvernekar@gmail.com>
2024-04-30 14:32:42 +0530

    add authentication in appwrite service

---
+++ BlogoSphere/src/appwrite/auth.js
@@ -0,0 +1,56 @@
+import config from "../config/config.js";
+
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+    
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.account= new Account(this.client)
+    }
+
+    async createAccount({email , password ,name}){
+        try{
+            const userAcount = await this.account.create(ID.unique(),email,password,name)
+            if(userAcount){
+                //call a method to login directly
+                this.login({email,password})
+            }else{
+                return error;
+            }
+        }catch(error){
+            throw error;
+        }
+
+    }
+    async login({email,password}){
+        try{
+            return await this.account.createEmailSession(email,password)    
+        }catch(error){
+            return error;
+        }
+    }
+    async getCurrentUser(){
+        try{
+            return await this.account.get();         
+        }catch(error){
+            console.log("Appwrite service :: getcurrentuser ::: error",error);
+        }
+        return null ;
+    }
+    async logout(){
+        try{
+            await this.account.deleteSessions()
+        }catch(error){
+            console.log("appwrite service:: logout:: error",error)
+        }
+    }
+}
+
+const authService = new AuthService
+
+export default authService;
