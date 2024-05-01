4c5c2c499d4d110e806c387d82eefb83633007dc
aaronvern <aaronvernekar@gmail.com>
2024-05-02 00:03:56 +0530

    add changes

--- BlogoSphere/src/components/index.js
+++ BlogoSphere/src/components/index.js
@@ -1,7 +1,10 @@
+import Container from "./container/Container";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
-
+import Logo from "./Login";
 export default {
     Footer,
-    Header
+    Header,
+    Container,
+    Logo
 }
