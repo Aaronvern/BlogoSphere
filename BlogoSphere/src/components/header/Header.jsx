eeb3b58c1850b11da4c99d70118ceb3f45d7d13b
aaronvern <aaronvernekar@gmail.com>
2024-05-01 23:01:07 +0530

    add input component

--- BlogoSphere/src/components/header/Header.jsx
+++ BlogoSphere/src/components/header/Header.jsx
@@ -41,16 +41,16 @@ const Header = () => {
             <nav className='flex'>
             <div className='mr-4'>
                 <Link to='/'>
-                <Logo width='70px'/>
+                    <Logo width='70px'/>
                 </Link>
             </div>
             <ul className='flex ml-auto'>
                 {navItems.map((item) => 
-                item.active ? (
+                    item.active ? (
                 <li key={item.name}>
                     <button
-                    onClick={() => navigate(item.slug)}
-                    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                        onClick={() => navigate(item.slug)}
+                        className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
                     >{item.name}</button>
                 </li>
                 ) : null
