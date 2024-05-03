41c82e685bc58ec461384b4299a75321cdad9a4e
aaronvern <aaronvernekar@gmail.com>
2024-05-03 22:15:41 +0530

    add editpost page

---
+++ BlogoSphere/src/pages/EditPost.jsx
@@ -0,0 +1,33 @@
+
+import Container from "../components/container/Container";
+import PostForm from "../components/post-form/PostForm";
+import React, {useEffect, useState} from 'react'
+import service from "../appwrite/conf.js";
+import { useNavigate,  useParams } from 'react-router-dom';
+
+function EditPost() {
+    const [post, setPosts] = useState(null)
+    const {slug} = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (slug) {
+            service.getPost(slug).then((post) => {
+                if (post) {
+                    setPosts(post)
+                }
+            })
+        } else {
+            navigate('/')
+        }
+    }, [slug, navigate])
+  return post ? (
+    <div className='py-8'>
+        <Container>
+            <PostForm post={post} />
+        </Container>
+    </div>
+  ) : null
+}
+
+export default EditPost
