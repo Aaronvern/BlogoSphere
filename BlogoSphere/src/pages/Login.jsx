6a3f696745ab0752361a0e0cc508ddd5360a1d3f
aaronvern <aaronvernekar@gmail.com>
2024-05-03 17:38:25 +0530

    add signup and login page

---
+++ BlogoSphere/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import Login from "../components/Login";
+
+import React from 'react'
+function LoginPage() {
+  return (
+    <div className='py-8'>
+        <Login />
+    </div>
+  )
+}
+
+export default LoginPage
