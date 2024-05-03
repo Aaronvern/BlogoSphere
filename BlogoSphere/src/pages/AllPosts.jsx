f6a65f9e30e0b0dfbffa60943ba1335d81888596
aaronvern <aaronvernekar@gmail.com>
2024-05-03 21:58:34 +0530

    add home and all posts page

---
+++ BlogoSphere/src/pages/AllPosts.jsx
@@ -0,0 +1,31 @@
+import PostCard from "../components/PostCard";
+import Container from "../components/container/Container";
+import service from '../appwrite/conf.js'
+
+import React, {useState, useEffect} from 'react'
+
+
+function AllPosts() {
+    const [posts, setPosts] = useState([])
+    useEffect(() => {}, [])
+    service.getPosts([]).then((posts) => {
+        if (posts) {
+            setPosts(posts.documents)
+        }
+    })
+  return (
+    <div className='w-full py-8'>
+        <Container>
+            <div className='flex flex-wrap'>
+                {posts.map((post) => (
+                    <div key={post.$id} className='p-2 w-1/4'>
+                        <PostCard {...post} />
+                    </div>
+                ))}
+            </div>
+            </Container>
+    </div>
+  )
+}
+
+export default AllPosts
